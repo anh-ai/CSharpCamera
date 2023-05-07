357dce9f62c76d07798345cae180ecedc0691d7d
Foxconn.AI Tuan Anh <tuananhktmt@gmail.com>
2023-05-07 16:42:43 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace C___Webcam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnOpenCam = new System.Windows.Forms.Button();
+            this.cboCamNames = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1143, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 604);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1143, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1143, 576);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.btnStart);
+            this.tabPage1.Controls.Add(this.pictureBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1135, 547);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(164, 454);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(40, 454);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(96, 37);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(40, 23);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(475, 412);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btnOpenCam);
+            this.tabPage2.Controls.Add(this.cboCamNames);
+            this.tabPage2.Controls.Add(this.pictureBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1135, 547);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenCam
+            // 
+            this.btnOpenCam.Location = new System.Drawing.Point(458, 54);
+            this.btnOpenCam.Name = "btnOpenCam";
+            this.btnOpenCam.Size = new System.Drawing.Size(96, 37);
+            this.btnOpenCam.TabIndex = 2;
+            this.btnOpenCam.Text = "button1";
+            this.btnOpenCam.UseVisualStyleBackColor = true;
+            this.btnOpenCam.Click += new System.EventHandler(this.btnOpenCam_Click);
+            // 
+            // cboCamNames
+            // 
+            this.cboCamNames.FormattingEnabled = true;
+            this.cboCamNames.Location = new System.Drawing.Point(79, 61);
+            this.cboCamNames.Name = "cboCamNames";
+            this.cboCamNames.Size = new System.Drawing.Size(364, 24);
+            this.cboCamNames.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(79, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(475, 412);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 626);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing_1);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button btnOpenCam;
+        private System.Windows.Forms.ComboBox cboCamNames;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using Emgu.CV;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV.Util;
+namespace C___Webcam
+{
+
+    public partial class Form1 : Form
+    {
+        private readonly TACamera mCam = new TACamera();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            GetFrametoPictureBox(sender, e);
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void GetFrametoPictureBox(object sender, EventArgs e)
+        {
+             Bitmap bitmap = mCam.GetFrame();
+            if (bitmap != null)
+            {
+                pictureBox2.Image = bitmap;
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click_1(object sender, EventArgs e)
+        {
+            mCam.initCam(0);
+            if (mCam.isCapturing)
+            {
+                Application.Idle += GetFrametoPictureBox;
+            }
+            else
+            {
+                Application.Idle -= GetFrametoPictureBox;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mCam.StopCam();
+            if (mCam.isCapturing)
+            {
+                Application.Idle += GetFrametoPictureBox;
+            }
+            else
+            {
+                Application.Idle -= GetFrametoPictureBox;
+            }
+        }
+
+        private void btnOpenCam_Click(object sender, EventArgs e)
+        {
+            mCam.initCam(0);
+            timer1.Start();
+        }
+    }
+
+    internal class TACamera
+    {
+        /* Emgu Install:
+         - Emgu.CV.runtime.windows
+         - If you are targeting .Net Framework, when using Emgu.CV.runtime.windows(.dldt / .cuda / .cuda.dldt) nuget packages for windows, 
+            please set the build architecture to either "x86" or "x64". Do not set the architecture to "Any CPU" 
+            Targeting .Net Framework: (https://www.emgu.com/wiki/index.php/Download_And_Installation#Using_the_Downloadable_packages)
+         * */
+        private VideoCapture capture;
+        public bool isCapturing = false;
+        Mat frame = new Mat();
+        Bitmap image;
+        public void initCam(int CamID = 0)
+        {
+            // Create a new VideoCapture object with the default camera
+            capture = new VideoCapture(CamID);
+            //capture.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, 800);
+            //capture.Set(Emgu.CV.CvEnum.CapProp.FrameHeight,600);
+            //capture.Set(Emgu.CV.CvEnum.CapProp.Exposure,50000);
+
+            // Start capturing frames
+            capture.Start();
+            isCapturing = true;
+        }
+        public Bitmap GetFrame()
+        {
+            _ = capture.Read(frame);
+            if (!frame.IsEmpty)
+            {
+                if (image != null)
+                {
+                    image.Dispose();
+                }
+                image = frame.ToBitmap();
+                return image;
+            }
+            return null;
+        }
+
+
+        public Bitmap GetFrame0()
+        {            
+            _ = capture.Read(frame);
+            if (!frame.IsEmpty)
+            {
+                image = frame.ToBitmap();
+
+                // Display the image in the PictureBox control
+                //pictureBox1.Image = image;
+                return image;
+            }
+            return null;
+        }
+        public void StopCam()
+        {
+            isCapturing = false;
+            capture.Stop();
+            capture.Dispose();            
+        }
+
+    }
+}
